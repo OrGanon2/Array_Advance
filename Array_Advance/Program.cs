8e5ba675c862881da2c43e6055f086b213ad5fba
Or Ganon <Or Ganon@DESKTOP-GD593R9>
2021-06-24 21:03:01 +0200

    Add project files.

---
+++ Array_Advance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Array_Advance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Multiply_board = new int[10,10];
+
+            for (int i = 1; i <= Multiply_board.GetLength(0); i++)
+            {
+                for (int j = 1; j <= Multiply_board.GetLength(1); j++)
+                {
+                    Console.Write(i * j + "\t");
+                }
+            }   Console.WriteLine();
+        }
+    }
+}
